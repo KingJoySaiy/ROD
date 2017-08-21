0380bf38b5ebb6361b9428760722841aaea6fca2
KingJoySaiy <15151972900@163.com>
2017-08-21 22:32:27 +0800

    Update POJ 1006.cpp

--- challenges/POJ 1006.cpp
+++ challenges/POJ 1006.cpp
@@ -1,3 +1,8 @@
+/*
+ * @模板题
+ * POJ 1006 （中国剩余定理）
+ * 题目大意：给定起始日期，三个活跃日期，三个周期，找出第一个共同活跃的日期。
+ */
 #include<iostream>
 #include<cstdio>
 typedef long long LL;
@@ -5,11 +10,11 @@ using namespace std;
 
 int a[3],m[3]={23,28,33},n=3;
 
-void extgcd(LL a,LL b,LL &x,LL &y){     //��չŷ���������Ԫ
+void extgcd(LL a,LL b,LL &x,LL &y){     //扩展欧几里德求逆元
 
     b?(extgcd(b,a%b,y,x),y-=(a/b)*x):(x=1,y=0);
 }
-LL crt(){      //�й�ʣ�ඨ��
+LL crt(){      //中国剩余定理
 
     LL M=1,res=0,x,y;
     for(int i=0;i<n;i++) M*=m[i];
@@ -18,7 +23,7 @@ LL crt(){      //�й�ʣ�ඨ��
         extgcd(mi,m[i],x,y);
         res=(res+mi*x*a[i])%M;
     }
-    if(res<0) res+=M;
+    if(res<0) res+=M;   //注意res<0的情况
     return res;
 }
 int main(){
@@ -31,7 +36,5 @@ int main(){
         printf("Case %d: the next triple peak occurs in %d days.\n",++ct,res);
     }
 
-return 0;
+    return 0;
 }
-
-
