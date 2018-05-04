6f00515bf853c1d277bffeedd4ff79de3cebc188
KingJoySaiy <1301679852@qq.com>
2018-05-04 17:06:35 +0800

    233

--- Geometry/基本函数/head.h
+++ Geometry/基本函数/head.h
@@ -14,7 +14,7 @@ const double eps = 1e-10;
 const double PI = acos(-1.0);
 const double inf = 1e12;
 
-int sgn(double x) {     //判断x是否为0
+int dcmp(double x) {     //判断x是否为0
 
     if(fabs(x) < eps) return 0;
     return x < 0 ? -1 : 1;
@@ -26,22 +26,25 @@ struct Point{           //定义点(向量)
     double len(){       //返回向量的长度
         return sqrt(x * x + y * y);
     }
-    Vector operator +(const Vector &b) const {     //向量+向量
-        return Vector(x + b.x, y - b.y);
+    Vector operator +(const Vector &b) const {       //向量+向量
+        return Vector(x + b.x, y + b.y);
     }
-    Vector operator -(const Point &b) const {     //向量-向量 or 点-点
+    Vector operator -(const Point &b) const {        //向量-向量 or 点-点
         return Vector(x - b.x, y - b.y);
     }
-    double operator ^(const Vector &b) const {    //向量的叉积
+    double operator ^(const Vector &b) const {       //向量的叉积
         return x*b.y - y*b.x;
     }
-    double operator *(const Vector &b) const {    //向量的点积
+    double operator *(const Vector &b) const {       //向量的点积
         return x*b.x + y*b.y;
     }
-    bool operator ==(const Point &b) const {
-        return !sgn(x - b.x) and !sgn(y - b.y);
+    Vector operator *(const double t) const {        //向量乘浮点数
+        return Vector(t * x, t * y);
     }
-    void transXY(double B) {    //绕原点左旋B（弧度）
+    bool operator ==(const Point &b) const {
+        return !dcmp(x - b.x) and !dcmp(y - b.y);
+    }
+    void trans_XY(double B) {    //绕原点左旋B（弧度）
         double tx = x,ty = y;
         x = tx*cos(B) - ty*sin(B);
         y = tx*sin(B) + ty*cos(B);
@@ -60,8 +63,8 @@ struct Line{
 
         //第一个值为0表示直线重合，为0表示重合，为1表示平行，为2是相交
         Point res = s;
-        if (sgn((s - e) ^ (b.s - b.e)) == 0){
-            if (sgn((s - b.e) ^ (b.s - b.e)) == 0) return make_pair(0, res);    //重合
+        if (dcmp((s - e) ^ (b.s - b.e)) == 0){
+            if (dcmp((s - b.e) ^ (b.s - b.e)) == 0) return make_pair(0, res);    //重合
             else return make_pair(1, res);              //平行
         }
         double t = ((s - b.s) ^ (b.s - b.e)) / ((s - e) ^ (b.s - b.e)); //用到定比分点
